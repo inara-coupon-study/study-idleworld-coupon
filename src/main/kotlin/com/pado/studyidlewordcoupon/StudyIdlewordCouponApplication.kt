153bff0b10895a8b7505fb8eb3268101f0f95bfd
hyo321 <hyo321@lguplus.co.kr>
2023-02-12 16:15:33 +0900

    first commit

---
+++ src/main/kotlin/com/pado/studyidlewordcoupon/StudyIdlewordCouponApplication.kt
@@ -0,0 +1,11 @@
+package com.pado.studyidlewordcoupon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudyIdlewordCouponApplication
+
+fun main(args: Array<String>) {
+	runApplication<StudyIdlewordCouponApplication>(*args)
+}
---
+++ src/test/kotlin/com/pado/studyidlewordcoupon/StudyIdlewordCouponApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pado.studyidlewordcoupon
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StudyIdlewordCouponApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
